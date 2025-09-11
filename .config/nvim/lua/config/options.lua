362467dd38027ffd86ce39a1a5b9c6047e9472bc
umar052001 <umar.111nawaz@gmail.com>
2025-09-12 02:57:03 +0500

    made my nvim cursor better (now every mode has that one rectangle big cursor) and also centered the nvim when at the end of screen add few lines for better readability

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,19 @@
 -- ~/.config/nvim/lua/config/options.lua
 
+-- keep at least 8 lines visible below the cursor
+vim.opt.scrolloff = 8
+
+-- recenter a bit when entering Insert at EOF
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		local lastline = vim.fn.line("$")
+		local currline = vim.fn.line(".")
+		if currline == lastline then
+			vim.cmd("normal! zz")
+		end
+	end,
+})
+vim.opt.guicursor = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
